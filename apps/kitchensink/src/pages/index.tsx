7596d7630dd32dbdee68871cfa4bae515d20326e
Andreas Adam <andreas.adam@composableweb.com>
2023-01-17 16:07:13 +0100

    chore: update deps to latest (#1)

--- apps/kitchensink/src/pages/index.tsx
+++ apps/kitchensink/src/pages/index.tsx
@@ -11,9 +11,9 @@ export default function Home() {
 
       <main className="mx-auto w-auto px-4 pt-16 pb-8 sm:pt-24 lg:px-8">
         <h1 className="mx-auto max-w-5xl text-center text-6xl font-extrabold leading-[1.1] tracking-tighter text-white sm:text-7xl lg:text-8xl xl:text-8xl">
-          Kitchensink <br className="hidden lg:block" />
+          Kitchensink ⏲ <br className="hidden lg:block" />
           <span className="inline-block bg-gradient-to-r from-red-500 to-blue-500 bg-clip-text text-transparent ">
-            Turborepo Example
+            Example
           </span>{" "}
         </h1>
         <div className="mx-auto mt-5 max-w-xl sm:flex sm:justify-center md:mt-8">
