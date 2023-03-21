2acc3e2ba9ee784678be3ad83ed9b521647deeb8
Andreas Adam <andreas.adam@composableweb.com>
2023-03-21 15:24:03 +0100

    grid layout on main layout

--- apps/kitchensink/src/app/layout.tsx
+++ apps/kitchensink/src/app/layout.tsx
@@ -9,8 +9,8 @@ import '@/styles/globals.css';
 const lato = Lato({ weight: ['400', '700'], subsets: ['latin'], variable: '--font-lato' });
 export default async function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html className={`${lato.variable} light`} lang="en">
-      <body className="flex h-full flex-col bg-zinc-50 dark:bg-black dark:text-base-200">
+    <html className={`${lato.variable} light h-full overflow-hidden`} lang="en">
+      <body className="grid h-full overflow-hidden bg-zinc-50 dark:bg-black dark:text-base-200">
         <header>
           <nav>
             <Link href="/">Home</Link>
