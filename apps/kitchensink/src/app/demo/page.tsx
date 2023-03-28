52566e6486518f1cb1321da11c7fb1c66df1f01b
Andreas Adam <andreas.adam@composableweb.com>
2023-03-28 15:35:50 +0200

    typography demo components vs prose

--- apps/kitchensink/src/app/demo/page.tsx
+++ apps/kitchensink/src/app/demo/page.tsx
@@ -1,8 +1,102 @@
+import { H2, Container, H1, H3, H4, H5, H6 } from '@prestyled/elements';
+
 const DemoPage = async () => {
   return (
-    <div>
-      <h1>demo</h1>
-    </div>
+    <Container>
+      <H1>demo</H1>
+      <div className="grid grid-cols-2 gap-4">
+        <div>
+          <H1>The Power of Visual Planning</H1>
+          <H2>Introduction</H2>
+          <p>
+            Effective planning is essential for achieving success in both personal and professional life. Planning can
+            be done in different ways, but visual planning is a powerful method that can help individuals to organize
+            their thoughts and ideas, set goals, and track progress. In this blog post, we will explore the benefits of
+            visual planning and how it can help individuals to achieve their objectives.
+          </p>
+          <h2>Enhanced Creativity</h2>
+          <p>
+            One of the primary benefits of visual planning is that it enhances creativity. When individuals engage in
+            visual planning, they are encouraged to think visually and creatively. This approach allows them to explore
+            different options and ideas that they may not have considered before. Visual planning stimulates the right
+            side of the brain, which is responsible for creativity, imagination, and intuition. By tapping into this
+            part of the brain, individuals can come up with innovative solutions to problems and challenges.
+          </p>
+          <h3>PROs:</h3>
+          <ul>
+            <li>Enhanced creativity</li>
+            <li>Improved problem-solving skills</li>
+            <li>Increased productivity</li>
+            <li>Improved memory</li>
+          </ul>
+          <h3>CONs:</h3>
+          <ul>
+            <li>Requires more time</li>
+            <li>Requires more effort</li>
+          </ul>
+          <hr />
+          <H1>One of the primary benefits of visual planning is that it enhances creativity</H1>
+          <H2>When individuals engage in visual planning, they are encouraged to think visually and creatively</H2>
+          <H3>
+            This approach allows them to explore different options and ideas that they may not have considered before
+          </H3>
+          <H4>
+            Visual planning stimulates the right side of the brain, which is responsible for creativity, imagination,
+            and intuitionV
+          </H4>
+          <H5>
+            By tapping into this part of the brain, individuals can come up with innovative solutions to problems and
+            challenges
+          </H5>
+          <H6>Effective planning is essential for achieving success in both personal and professional life</H6>
+        </div>
+        <div className="dark:prose-inverted prose">
+          <h1>The Power of Visual Planning</h1>
+          <h2>Introduction</h2>
+          <p>
+            Effective planning is essential for achieving success in both personal and professional life. Planning can
+            be done in different ways, but visual planning is a powerful method that can help individuals to organize
+            their thoughts and ideas, set goals, and track progress. In this blog post, we will explore the benefits of
+            visual planning and how it can help individuals to achieve their objectives.
+          </p>
+          <h2>Enhanced Creativity</h2>
+          <p>
+            One of the primary benefits of visual planning is that it enhances creativity. When individuals engage in
+            visual planning, they are encouraged to think visually and creatively. This approach allows them to explore
+            different options and ideas that they may not have considered before. Visual planning stimulates the right
+            side of the brain, which is responsible for creativity, imagination, and intuition. By tapping into this
+            part of the brain, individuals can come up with innovative solutions to problems and challenges.
+          </p>
+          <h3>PROs:</h3>
+          <ul>
+            <li>Enhanced creativity</li>
+            <li>Improved problem-solving skills</li>
+            <li>Increased productivity</li>
+            <li>Improved memory</li>
+          </ul>
+          <h3>CONs:</h3>
+          <ul>
+            <li>Requires more time</li>
+            <li>Requires more effort</li>
+          </ul>
+          <hr />
+          <h1>One of the primary benefits of visual planning is that it enhances creativity</h1>
+          <h2>When individuals engage in visual planning, they are encouraged to think visually and creatively</h2>
+          <h3>
+            This approach allows them to explore different options and ideas that they may not have considered before
+          </h3>
+          <h4>
+            Visual planning stimulates the right side of the brain, which is responsible for creativity, imagination,
+            and intuitionV
+          </h4>
+          <h5>
+            By tapping into this part of the brain, individuals can come up with innovative solutions to problems and
+            challenges
+          </h5>
+          <h6>Effective planning is essential for achieving success in both personal and professional life</h6>
+        </div>
+      </div>
+    </Container>
   );
 };
 
